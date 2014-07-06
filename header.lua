1b85e55af456187ea942aa062615e88bdc22d76b
Ciaran Downey <me@ciarand.me>
2014-07-06 13:42:57 -0700

    Make the header logic work better

--- header.lua
+++ header.lua
@@ -6,24 +6,50 @@ local P, R, S, V, C, Cg, Cb, Cmt, Cc, Cf, Ct, B, Cs =
 
 local exports = {}
 
--- part groups
-local eol   = P("\n")
-local equal = P("=")
-local dash  = P("-")
+local any            = P(1)
+local fail           = any - 1
+local always         = P("")
+local eof            = - any
 
--- capture groups
--- capture a string up until any number of eols
-local line    = C((1 - eol) ^1)
+local spacechar      = P("\t ")
+local newline        = P("\n")
+local hash           = P("#")
+local equal          = P("=")
+local dash           = P("-")
 
-exports.headerNode = function(str)
-    return { title = str }
+
+local optionalspace  = spacechar^0
+local spaces         = spacechar^1
+local linechar       = P(1 - newline)
+
+local blankline      = optionalspace * newline / "\n"
+local blanklines     = blankline^0
+local line           = linechar^0 * newline + linechar^1 * eof
+local nonemptyline   = line - blankline
+
+exports.headerNode = function(str, level)
+    if level == nil then l = 1
+    elseif level > 6 then l = 6
+    else l = level end
+
+    return { title = str, level = l }
 end
 
 exports.parseHeaderNode = function(str)
-    local header = lpeg.match(line, str)
-    local hlen = string.len(header)
+    local HeadingLevel = equal^1 * Cc(1) + dash^1 * Cc(2)
 
-    return { title = header }
+    local rules = #(line * equal)
+                * C(line - newline)
+                * HeadingLevel
+                * optionalspace * newline^0
+                / function(s, l) return { title = s:gsub("(.-)%s*$", "%1"), level = l } end
+
+    local res = lpeg.match(rules, str)
+    if res ~= nil then
+        return res
+    end
+
+    error(string.format("parse_blocks failed on:\n%s", str:sub(1,20)))
 end
 
 return exports
